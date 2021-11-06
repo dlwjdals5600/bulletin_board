67c90dd7d2ab0ab61408f0796c4a3957987fb15b
이정민 <dlwjdals5600@gmail.com>
2021-11-06 13:00:53 +0900

    Create : accounts-signup

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,10 @@
 from django.db   import models
 from django.conf import settings
 
+Account = settings.AUTH_USER_MODEL
 
 class Post(models.Model):
-    author     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    author     = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
     title      = models.CharField(max_length=255)
     content    = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
